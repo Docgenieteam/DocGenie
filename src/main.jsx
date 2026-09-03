c6e9b7e40e8c3734fad241156e74854bb78ef9e7
Janhavi110r <janhavipawshe5@gmail.com>
2026-09-03 22:34:29 +0530

    deplayment changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "./styles.css";
 
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
